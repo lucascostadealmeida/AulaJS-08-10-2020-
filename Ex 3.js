63b8e7e8271a73c1514b11f9bd9db94ae0049f9f
Lucas Costa de Almeida <lucascostadealmeida007@gmail.com>
2020-10-30 14:56:36 -0300

    Modification in file

--- Ex 3.js
+++ Ex 3.js
@@ -1,12 +1,4 @@
-/*Desenvolva uma aplicação em JavaScript para ler um salário e atualizá-lo de acordo
-com a tabela abaixo.
-Faixa salárial Aumento
-até 
-1.045,00            30%
-1.045,01 a 1.500,00 25%
-1.500,01 a 2.600,00 20%
-2.600,01 a 3.550,00 15%
-Acima de 3.550,00   10% */
+
 
 const aumento1 = 0.3;
 const aumento2 = 0.25;
@@ -46,17 +38,17 @@ else if ((salario > 1045.00) && (salario <= 1500.00)) {
     console.log("Seu salário foi atualizado para: " + salario_final2.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}));
 }
 
-//imprimindo o aumento salarial de 20%
-else if ((salario > 1500.00) && (salario <= 2600.00)) {
-    console.log("Seu salário foi atualizado para: " + salario_final3.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2})); 
-}
+// //imprimindo o aumento salarial de 20%
+// else if ((salario > 1500.00) && (salario <= 2600.00)) {
+//     console.log("Seu salário foi atualizado para: " + salario_final3.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2})); 
+// }
 
-//imprimindo o aumento salarial de 15%
-else if ((salario > 2600.00) && (salario <= 3550.00)) {
-    console.log("Seu salário foi atualizado para: " + salario_final4.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}));
-}
 
-//imprimindo o aumento salarial de 10%
-else if (salario > 3550.00) {
-    console.log("Seu salário foi atualizado para: " + salario_final5.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}));
-}
+// else if ((salario > 2600.00) && (salario <= 3550.00)) {
+//     console.log("Seu salário foi atualizado para: " + salario_final4.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}));
+// }
+
+
+// else if (salario > 3550.00) {
+//     console.log("Seu salário foi atualizado para: " + salario_final5.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2}));
+// }
